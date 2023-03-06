e1598f907e84b70b102023b5f243c082dd410a48
MariaRantanen <122267909+MariaRantanen@users.noreply.github.com>
2023-03-06 11:14:22 +0200

    kuva kentän lisäys

--- blogi/models.py
+++ blogi/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
-    kuva = models.ImageField(null=True)
+    kuva = models.ImageField(upload_to="blogi/kuvat", null=True)
     luotu = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
