e1a0cebca7b8289b597b6fbbfb74073ef62e376d
MariaRantanen <122267909+MariaRantanen@users.noreply.github.com>
2023-03-06 09:13:21 +0200

    jotain

--- blogi/models.py
+++ blogi/models.py
@@ -4,7 +4,7 @@ class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
     luotu = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.otsikko
         
