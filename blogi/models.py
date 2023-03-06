aac7896aff6b9a23f2c97a9bbecb18bc322d515e
MariaRantanen <122267909+MariaRantanen@users.noreply.github.com>
2023-03-06 09:45:51 +0200

    kuva muuttujan lisäys

--- blogi/models.py
+++ blogi/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
+    kuva = models.ImageField(null=True)
     luotu = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.otsikko
         
